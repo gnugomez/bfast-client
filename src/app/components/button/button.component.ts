ef3737676ffe4b14a9912691eb468de45de47037
Jordi Gómez Hidalgo <gnugomez@gmail.com>
2021-12-14 19:58:07 +0100

    fixed button not being disabled on production

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,18 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'button.app-button, app-button',
+  selector: 'button[app-button], app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
   @Input() variant = 'primary';
-  @Input() disabled = false;
   @Input() loading = false;
   @Input() size = 'md';
   @Input() link = '';
   @Input() target = '_self';
   @Input() classes = '';
+  @HostBinding('attr.disabled') disabledAttribute = Input();
+  disabled = false;
 
   constructor() {}
 
