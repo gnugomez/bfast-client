2bef91981483f338e9b7535434671f29a5dc6b0d
Jordi Gómez Hidalgo <gnugomez@gmail.com>
2022-01-26 20:08:42 +0100

    feat(layout): notifications submenu

--- src/app/layouts/components/notifications/notifications.component.ts
+++ src/app/layouts/components/notifications/notifications.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import toggleAnimation from '../../animations/toggleAnimation';
 
 @Component({
   selector: 'layout-notifications',
   templateUrl: './notifications.component.html',
+  animations: [toggleAnimation],
   styleUrls: ['./notifications.component.scss'],
 })
 export class NotificationsComponent implements OnInit {
