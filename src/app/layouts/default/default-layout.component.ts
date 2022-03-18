368c1019378968c0265d5be29bb2c2535a680416
Jordi Gómez Hidalgo <gnugomez@gmail.com>
2022-03-18 08:16:00 +0100

    feat(sidebar): close sidebar on route change

--- src/app/layouts/default/default-layout.component.ts
+++ src/app/layouts/default/default-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'layout-default',
@@ -6,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./default-layout.component.scss'],
 })
 export class DefaultLayoutComponent implements OnInit {
+
+  public constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.sidebarIsOpen = false;
+      }
+    });
+  }
+
   public sidebarIsOpen = false;
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   public openSidebar(): void {
     this.sidebarIsOpen = true;
